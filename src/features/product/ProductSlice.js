4d7f5090dbf9ddd7e289873706ed87aa7397bf8b
mohamed00Sayed <sayedm22529@gmail.com>
2023-03-02 20:57:29 +0200

    add sorting functionality to retrieved product skus

--- src/features/product/ProductSlice.js
+++ src/features/product/ProductSlice.js
@@ -3,7 +3,7 @@ import {
   createEntityAdapter,
   createSlice,
 } from "@reduxjs/toolkit";
-import API  from "./productAPI";
+import API from "./productAPI";
 
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
@@ -11,9 +11,10 @@ export const fetchProducts = createAsyncThunk(
     return await API.fetchProducts(url);
   }
 );
-
+/*normalize the data using entity adapter*/
 export const productsAdapter = createEntityAdapter({
   selectId: (product) => product.sku,
+  sortComparer: (a, b) => a.sku.localeCompare(b.sku),
 });
 
 const initialState = productsAdapter.getInitialState();

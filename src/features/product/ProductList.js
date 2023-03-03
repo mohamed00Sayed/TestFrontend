b2d1d705361ae8245436c2aa9afa3eb0c8166478
mohamed00Sayed <sayedm22529@gmail.com>
2023-03-03 11:42:59 +0200

    modify page structure for correct styling, add mass delete list

--- src/features/product/ProductList.js
+++ src/features/product/ProductList.js
@@ -1,14 +1,28 @@
 import { useSelector } from "react-redux";
 import Product from "./Product";
+import "./products.scss";
 
 const ProductList = () => {
   const ids = useSelector((state) => state.products.ids);
   const products = useSelector((state) => state.products.entities);
+  const massDeleteList = [];
 
   return (
-    <ul>
-        {ids.map((id) => <Product product={products[id]}/>)}
-    </ul>
+    <div id="pl-page">
+      <div id="pl-header">
+        <div id="pl-title">Product List</div>
+        <div className="buttons">
+          <button className="button">ADD</button>
+          <button className="button">MASS DELETE</button>
+        </div>
+      </div>
+      <hr/>
+      <ul>
+        {ids.map((id) => (
+          <Product product={products[id]} list={massDeleteList}/>
+        ))}
+      </ul>
+    </div>
   );
 };
 

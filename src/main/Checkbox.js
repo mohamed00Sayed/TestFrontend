1f9c9e15080c41d234b0cea6b2cece0cdd6a4a11
mohamed00Sayed <sayedm22529@gmail.com>
2023-03-03 15:25:49 +0200

    fix: get the correct value from the event target

--- src/main/Checkbox.js
+++ src/main/Checkbox.js
@@ -1,7 +1,7 @@
 import "../styles/checkbox.scss";
 const Checkbox = ({ addToList, removeFromList }) => {
   const changeList = (event) => {
-    const checked = event.target.value;
+    const checked = event.target.checked;
     if (checked) {
       addToList();
     } else {
@@ -14,7 +14,7 @@ const Checkbox = ({ addToList, removeFromList }) => {
       <div className="cbx">
         <input id="cbx-12" type="checkbox" onChange={changeList} />
         <label htmlFor="cbx-12"></label>
-        <svg width="15" height="14" viewbox="0 0 15 14" fill="none">
+        <svg width="15" height="14" viewBox="0 0 15 14" fill="none">
           <path d="M2 8.36364L6.23077 12L13 2"></path>
         </svg>
       </div>
@@ -24,7 +24,7 @@ const Checkbox = ({ addToList, removeFromList }) => {
           <filter id="goo-12">
             <fegaussianblur
               in="SourceGraphic"
-              stddeviation="4"
+              stdDeviation="4"
               result="blur"
             ></fegaussianblur>
             <fecolormatrix
